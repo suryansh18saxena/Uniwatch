a06a2186c38b77277ed6c85900f0d3c47ecc61c7
suryansh1808 <suryansh.saxena_cs24@gla.ac.in>
2026-04-10 18:00:59 +0530

    added modes to the admin dashboard

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Server
 
-# Register your models here.
+
+@admin.register(Server)
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ip_address', 'ssh_user', 'setup_status', 'is_active', 'has_containers', 'created_at')
+    list_filter = ('setup_status', 'is_active', 'has_containers')
+    search_fields = ('name', 'ip_address')
+    readonly_fields = ('setup_logs', 'created_at', 'updated_at')
