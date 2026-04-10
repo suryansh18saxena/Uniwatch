bff727adaa5c54959ad1af264473a87f3f7e8fcc
suryansh1808 <suryansh.saxena_cs24@gla.ac.in>
2026-04-10 17:59:06 +0530

    created the path for all

--- monitor/urls.py
+++ monitor/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
     path('add-server/', views.add_server, name='add_server'),
+    path('server/<int:server_id>/', views.server_detail, name='server_detail'),
+    path('server/<int:server_id>/delete/', views.delete_server, name='delete_server'),
+    path('server/<int:server_id>/retry/', views.retry_setup, name='retry_setup'),
 ]
